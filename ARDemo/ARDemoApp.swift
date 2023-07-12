b6b14a97ff69ac86cf2ba40da97af5e1191d37ed
Amadougaye Cissé <amadougaye.cisse@zuehlke.com>
2023-07-12 10:57:39 +0200

    Initial Commit

---
+++ ARDemo/ARDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARDemoApp.swift
+//  ARDemo
+//
+//  Created by Amadougaye Cisse on 12.07.23.
+//
+
+import SwiftUI
+
+@main
+struct ARDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
