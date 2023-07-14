f6387ca257e97145c53909e54f5d1755cf25f1e2
Amadougaye Cissé <amadougaye.cisse@zuehlke.com>
2023-07-14 13:07:37 +0200

    Adding AR Object Display menu item

--- ARDemo/View/MainMenuView.swift
+++ ARDemo/View/MainMenuView.swift
@@ -9,10 +9,10 @@ import SwiftUI
 
 struct MainMenuView: View {
     let options = [
-        OptionCardData(title: "AR Object Display", iconName: "cube.fill"),
-        OptionCardData(title: "Bring Objects into AR", iconName: "camera.fill"),
-        OptionCardData(title: "Parametric 3D Room Scans", iconName: "house.fill"),
-        OptionCardData(title: "Manipulate USDZ Files", iconName: "wand.and.stars.inverse")
+        OptionCardData(title: "AR Object Display", iconName: "cube.fill", destination: AnyView(ARObjectDisplayView())),
+        OptionCardData(title: "Bring Objects into AR", iconName: "camera.fill", destination: nil),
+        OptionCardData(title: "Parametric 3D Room Scans", iconName: "house.fill", destination: nil),
+        OptionCardData(title: "Manipulate USDZ Files", iconName: "wand.and.stars.inverse", destination: nil),
     ]
 
     var body: some View {
@@ -20,9 +20,17 @@ struct MainMenuView: View {
             ScrollView {
                 LazyVStack(spacing: 16) {
                     ForEach(options) { option in
-                        OptionCard(option: option)
-                            .frame(maxWidth: .infinity)
-                            .padding(.horizontal)
+                        NavigationLink(destination: {
+                            if let destination = option.destination {
+                                destination
+                            } else {
+                                Text("Feature not yet implemented")
+                            }
+                        }) {
+                            OptionCard(option: option)
+                                .frame(maxWidth: .infinity)
+                                .padding(.horizontal)
+                        }
                     }
                 }
                 .padding(.vertical)
@@ -45,6 +53,24 @@ struct OptionCard: View {
             Text(option.title)
                 .font(.headline)
                 .padding()
+            
+            Spacer()
+        
+            if option.destination != nil {
+                Image(systemName: "checkmark.circle.fill")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .foregroundColor(.green)
+                    .frame(width: 20, height: 20)
+                    .padding()
+            } else {
+                Image(systemName: "xmark.circle.fill")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .foregroundColor(.red)
+                    .frame(width: 20, height: 20)
+                    .padding()
+            }
         }
         .frame(maxWidth: .infinity, alignment: .leading)
         .background(Color.white)
@@ -57,7 +83,9 @@ struct OptionCardData: Identifiable {
     let id = UUID()
     let title: String
     let iconName: String
+    let destination: AnyView?
 }
+
 
 
 
