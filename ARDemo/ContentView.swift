923e267dd78bffabbb7c2c132b45d219e6b8730c
Amadougaye Cissé <amadougaye.cisse@zuehlke.com>
2023-07-14 09:16:36 +0200

    Adding AR Capabilities to place a cube, a sphere, a cylinder

--- ARDemo/ContentView.swift
+++ ARDemo/ContentView.swift
@@ -8,14 +8,80 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject private var arView = CustomARView()
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            CustomARViewRep(arView: arView)
+                .ignoresSafeArea()
+            
+            VStack {
+                Spacer()
+                
+                HStack {
+                    Button(action: {
+                        arView.placeBlueBlock()
+                    }) {
+                        HStack {
+                            Image(systemName: "cube.fill")
+                                .foregroundColor(.white)
+                        }
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                    }
+                    
+                    Spacer()
+                    
+                    Button(action: {
+                        arView.placeGreenCylinder()
+                    }) {
+                        HStack {
+                            Image(systemName: "cylinder.fill")
+                                .foregroundColor(.white)
+                        }
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                    }
+                    
+                    Spacer()
+                    
+                    Button(action: {
+                        arView.placeRedSphere()
+                    }) {
+                        HStack {
+                            Image(systemName: "circle.fill")
+                                .foregroundColor(.white)
+                                
+                        }
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                    }
+                    
+                    Spacer()
+                    
+                    Button(action: {
+                        arView.placeYellowText()
+                    }) {
+                        HStack {
+                            Image(systemName: "character")
+                                .foregroundColor(.white)
+                        }
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                    }
+                }
+                .offset(y: -30)
+                .padding(EdgeInsets(top: 0, leading: 10, bottom: 0, trailing: 10))
+            }
         }
-        .padding()
     }
 }
 
